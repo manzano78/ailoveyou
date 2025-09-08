e6be6486f589fa9e99a5ca15eda296b97372b71b
manzano78 <mikael.anzano@gmail.com>
2025-09-08 15:09:48 +0200

    cors fix

--- app/routes/api/base-api.ts
+++ app/routes/api/base-api.ts
@@ -1,6 +1,11 @@
 import type { unstable_MiddlewareFunction } from 'react-router';
 // import { provideAuthenticatedUserId } from '~/routes/api/auth';
 
+const allowedOrigins: Array<string | RegExp> = [
+  'https://www.meetic.fr',
+  /^https:\/\/([a-z0-9-]+)\.www\.meetic\.fr\.kube\.recette\.ilius\.io$/,
+];
+
 // const securityMiddleware: unstable_MiddlewareFunction<Response> = async (
 //   { request },
 //   next,
@@ -12,21 +17,34 @@ import type { unstable_MiddlewareFunction } from 'react-router';
 //   });
 // };
 
+const isAllowedOrigin = (origin: string): boolean => {
+  return allowedOrigins.some((allowedOrigin) =>
+    typeof allowedOrigin === 'string'
+      ? origin === allowedOrigin
+      : allowedOrigin.test(origin),
+  );
+};
+
 const corsMiddleware: unstable_MiddlewareFunction<Response> = async (
-  _,
+  { request },
   next,
 ) => {
-  const response = await next();
+  const origin = request.headers.get('Origin');
 
-  response.headers.set('Access-Control-Allow-Origin', 'https://www.meetic.fr');
-  response.headers.set('Access-Control-Allow-Credentials', 'true');
-  response.headers.set('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
-  response.headers.set(
-    'Access-Control-Allow-Headers',
-    'Content-Type, Authorization',
-  );
+  if (origin && isAllowedOrigin(origin)) {
+    const response = await next();
 
-  return response;
+    response.headers.set('Access-Control-Allow-Origin', origin);
+    response.headers.set('Access-Control-Allow-Credentials', 'true');
+    response.headers.set('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+    response.headers.set(
+      'Access-Control-Allow-Headers',
+      'Content-Type, Authorization',
+    );
+    response.headers.append('Vary', 'Origin');
+
+    return response;
+  }
 };
 
 export const unstable_middleware: unstable_MiddlewareFunction<Response>[] = [
