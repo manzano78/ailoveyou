27ad54e7d1f8945edc06b09c24cceffe862b91dd
manzano78 <mikael.anzano@gmail.com>
2025-09-25 16:46:50 +0200

    attempt cors fix

--- app/server.ts
+++ app/server.ts
@@ -6,5 +6,16 @@ const handle = createRequestHandler(build);
 export default async function handleRequest(
   request: Request,
 ): Promise<Response> {
+  if (request.method.toUpperCase() === 'OPTIONS') {
+    return new Response(null, {
+      status: 204,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+        'Access-Control-Allow-Headers': '*',
+      },
+    });
+  }
+
   return handle(request, new RouterContextProvider());
 }
