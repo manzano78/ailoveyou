0d28d8a725358b74ca97bbb72a5dbcaa9e7f336d
manzano78 <mikael.anzano@gmail.com>
2025-09-26 14:42:29 +0200

    deactivate temp api limitation

--- app/routes/api/base-api.ts
+++ app/routes/api/base-api.ts
@@ -1,10 +1,10 @@
 import { corsMiddleware } from '~/infra/cors';
 import { apiAuthMiddleware } from '~/infra/authentication';
-import { apiLimitationMiddleware } from '~/infra/api-limitation/api-limitation-middleware';
+// import { apiLimitationMiddleware } from '~/infra/api-limitation/api-limitation-middleware';
 import type { MiddlewareFunction } from 'react-router';
 
 export const middleware: MiddlewareFunction<Response>[] = [
   apiAuthMiddleware,
-  apiLimitationMiddleware,
+  // apiLimitationMiddleware,
   corsMiddleware,
 ];
